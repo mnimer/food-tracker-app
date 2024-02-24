87d473aecf125b0fbde327ba286d2ccd8195662f
Mike Nimer <mnimer@gmail.com>
2024-02-24 15:36:00 -0600

    Upgrade versions and add image picker, camera buttons

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
-import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:list_collections/firebase_options.dart';
--- app/lib/widgets/log_food_picture_bottom_sheet.dart
+++ app/lib/widgets/log_food_picture_bottom_sheet.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+//import 'package:image_picker/image_picker.dart';
 
 class LogFoodPictureBottomSheet extends StatefulWidget {
-  const LogFoodPictureBottomSheet({Key? key}) : super(key: key);
+  const LogFoodPictureBottomSheet({super.key});
 
   @override
   State<LogFoodPictureBottomSheet> createState() => _LogFoodPictureBottomSheetState();
@@ -15,25 +16,62 @@ class _LogFoodPictureBottomSheetState extends State<LogFoodPictureBottomSheet> {
 
   @override
   Widget build(BuildContext context) {
-    return  Row(
-      children: [
-        OutlinedButton(onPressed: () async {
-          image = await picker.pickImage(source: ImageSource.gallery);
-          if (image != null) {
-            print(image!.path);
-          }
-        },
-       child: const Text('pick image'),),
-       Builder(
-        builder: (context) {
-          if( image == null ){
-            //return const Text('');
-          }
+    return  Column(children:[
+      Row(
+        children: [
+          OutlinedButton(onPressed: () async {
+            image = await picker.pickImage(source: ImageSource.gallery);
+            if (image != null) {
+              print(image!.path);
+            }
+          },
+         child: const Text('pick image'),),
+         Builder(
+          builder: (context) {
+            if( image == null ){
+              //return const Text('');
+            }
 
-          return Text(image?.path ?? '');
-        }
-       )
-      ],
-    );
+            return Text(image?.path ?? '');
+          }
+         )
+        ],
+      ),
+      Row(
+        children: [
+          OutlinedButton(onPressed: () async {
+            image = await picker.pickImage(source: ImageSource.camera);
+            if (image != null) {
+              print(image!.path);
+            }
+          },
+            child: const Text('take picture'),),
+          Builder(
+              builder: (context) {
+                if( image == null ){
+                  //return const Text('');
+                }
+
+                return Text(image?.path ?? '');
+              }
+          )
+        ],
+      ),
+      Row(
+        children: [
+          const OutlinedButton(onPressed: null,
+            child: Text('scan barcode'),),
+          Builder(
+              builder: (context) {
+                if( image == null ){
+                  //return const Text('');
+                }
+
+                return Text(image?.path ?? '');
+              }
+          )
+        ],
+      )
+    ]);
   }
 }
